6c6bc1ae8cb22ffec48dfb60e9154ee5cc2ffcac
Yehuda Cohen <cohen72@gmail.com>
2021-09-19 18:39:18 +0300

    initializing project and adding all assets

---
+++ Nanit/Global/Colors.swift
@@ -0,0 +1,25 @@
+//
+//  NANTextField.swift
+//  Nanit
+//
+//  Created by Yehuda Oren Cohen on Sep 19, 2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  public static var darkGreyBlue = Color("darkGreyBlue")
+  public static var darkBlue = Color("darkBlue")
+  public static var blush = Color("blush")
+  public static var pale = Color("pale")
+  public static var lightBlueGrey = Color("lightBlueGrey")
+  public static var lightBlueGreyTwo = Color("lightBlueGreyTwo")
+  public static var lightTeal = Color("lightTeal")
+  public static var paleTeal = Color("paleTeal")
+  public static var sky = Color("sky")
+  public static var textFieldPlaceholder = Color("textFieldPlaceholder")
+  public static var textFieldBorder = Color("textFieldBorder")
+  public static var bodyGray = Color("bodyGray")
+  public static var goldenYellow = Color("goldenYellow")
+}
---
+++ Nanit/NanitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanitApp.swift
+//  Nanit
+//
+//  Created by Yehuda Oren Cohen on Sep 14, 2021.
+//
+
+import SwiftUI
+
+@main
+struct NanitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DetailsView()
+        }
+    }
+}
